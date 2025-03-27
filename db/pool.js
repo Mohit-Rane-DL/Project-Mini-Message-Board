f8efa1d25eb5c2a3c011a24b1d90578749ba3bf0
Mohit Rane <182085286+Mohit-Rane-DL@users.noreply.github.com>
2025-03-27 16:11:26 +0530

    Deployment

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 module.exports = new Pool({
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
