603f96b11b79e9d3710d80b74ec047cfa8463d5c
Mohit Rane <182085286+Mohit-Rane-DL@users.noreply.github.com>
2025-04-02 15:15:41 +0530

    Deployment

--- db/pool.js
+++ db/pool.js
@@ -5,5 +5,5 @@ module.exports = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false, // This allows you to connect to Railway's private database with SSL enabled
-      },
+      }
 });
